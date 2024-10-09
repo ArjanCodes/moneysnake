76ed0aaefe8d057017c4e47aa1c4f82e0047fecf
Arjan Egges <arjan@quarterfall.com>
2024-10-09 17:43:58 +0200

    Added exclude_none option. Changed endpoint into a property.

--- moneysnake/model.py
+++ moneysnake/model.py
@@ -4,38 +4,48 @@ from typing import Any, Optional, Self
 from .client import post_request
 
 
-def to_endpoint(class_name: str) -> str:
-    return "".join(
-        ["_" + letter.lower() if letter.isupper() else letter for letter in class_name]
-    ).lstrip("_")
-
-
 @dataclass
 class MoneybirdModel:
     id: Optional[int] = None
 
-    def to_dict(self) -> dict[str, Any]:
-        return {key: value for key, value in self.__dict__.items() if value is not None}
+    @property
+    def endpoint(self) -> str:
+        return "".join(
+            [
+                "_" + letter.lower() if letter.isupper() else letter
+                for letter in self.__class__.__name__
+            ]
+        ).lstrip("_")
+
+    def to_dict(self, exclude_none: bool = False) -> dict[str, Any]:
+        def convert_value(value: Any) -> Any:
+            if isinstance(value, MoneybirdModel):
+                return value.to_dict()
+            return value
+
+        return {
+            key: convert_value(value)
+            for key, value in self.__dict__.items()
+            if not (exclude_none and value is None)
+        }
 
     def load(self, id: int) -> None:
-        endpoint = to_endpoint(self.__class__.__name__)
-        data = post_request(f"{endpoint}s/{id}", method="get")
+        data = post_request(f"{self.endpoint}s/{id}", method="get")
         self.update(data)
 
     def save(self) -> None:
-        endpoint = to_endpoint(self.__class__.__name__)
         if self.id is None:
             data = post_request(
-                f"{endpoint}s",
-                data={endpoint: self.to_dict()},
+                f"{self.endpoint}s",
+                data={self.endpoint: self.to_dict()},
                 method="post",
             )
             # update the current object with the data
             self.update(data)
         else:
             data = post_request(
-                f"{endpoint}s/{self.id}",
-                data={endpoint: self.to_dict()},
+                f"{self.endpoint}s/{self.id}",
+                data={self.endpoint: self.to_dict()},
                 method="patch",
             )
             self.update(data)
@@ -46,10 +56,9 @@ class MoneybirdModel:
                 setattr(self, key, value)
 
     def delete(self) -> None:
-        endpoint = to_endpoint(self.__class__.__name__)
         if not self.id:
             raise ValueError("Contact has no id.")
-        post_request(f"{endpoint}s/{self.id}", method="delete")
+        post_request(f"{self.endpoint}s/{self.id}", method="delete")
         # remove the id from the object
         self.id = None
 
@@ -61,16 +70,16 @@ class MoneybirdModel:
 
     @classmethod
     def update_by_id(cls: type[Self], id: int, data: dict[str, Any]) -> Self:
-        contact = cls.find_by_id(id)
-        contact.update(data)
-        contact.save()
-        return contact
+        entity = cls(id)
+        entity.update(data)
+        entity.save()
+        return entity
 
     @classmethod
-    def delete_by_id(cls: type[Self], contact_id: int) -> Self:
-        contact = cls.find_by_id(contact_id)
-        contact.delete()
-        return contact
+    def delete_by_id(cls: type[Self], id: int) -> Self:
+        entity = cls(id)
+        entity.delete()
+        return entity
 
     @classmethod
     def from_dict(cls: type[Self], data: dict[str, Any]) -> Self:
