436f2f40cb679725ecf46b74c72584cc6ef3c263
Tim Evci <tevci93@gmail.com>
2024-11-19 16:46:30 +0300

    feat: enhance from_dict method for type conversion and improve test assertions for invoice ID and payment price

--- moneysnake/model.py
+++ moneysnake/model.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, Optional, Self
+from typing import Any, Optional, Self, Union, get_origin, get_args
 import inspect
 
 from .client import post_request
@@ -84,6 +84,22 @@ class MoneybirdModel:
 
     @classmethod
     def from_dict(cls: type[Self], data: dict[str, Any]) -> Self:
-        return cls(
-            **{k: v for k, v in data.items() if k in inspect.signature(cls).parameters}
-        )
+        params = inspect.signature(cls).parameters
+        for key, value in data.items():
+            if key in params:
+                param_type = params[key].annotation
+                # check if the parameter is optional
+                if param_type is not inspect.Parameter.empty:
+                    origin = get_origin(param_type)
+                    args = get_args(param_type)
+                    # check if the parameter is a Union with None
+                    if origin is Union and type(None) in args:
+                        # get the first non-None type
+                        param_type = next(arg for arg in args if arg is not type(None))
+                    try:
+                        # try to convert the value to the parameter type
+                        data[key] = param_type(value)
+                    except (ValueError, TypeError):
+                        pass
+
+        return cls(**{k: v for k, v in data.items() if k in params})
