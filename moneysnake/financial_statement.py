6a29ea696eb73c7e8a20786922f9e087411475f6
TimEvci <tevci93@gmail.com>
2024-11-19 12:49:01 +0300

    feat: move financial mutation to separate file and update financial statement.

--- moneysnake/financial_statement.py
+++ moneysnake/financial_statement.py
@@ -1,38 +1,8 @@
-from model import MoneybirdModel
-
+from typing import List, Optional, Self
 from dataclasses import dataclass, field
-from typing import List, Optional
 
-
-@dataclass
-class FinancialMutation:
-    """
-    Represents a financial mutation in Moneybird.
-    """
-
-    id: Optional[str] = None
-    administration_id: Optional[str] = None
-    amount: Optional[str] = None
-    code: Optional[str] = None
-    date: Optional[str] = None
-    message: Optional[str] = None
-    contra_account_name: Optional[str] = None
-    contra_account_number: Optional[str] = None
-    state: Optional[str] = None
-    amount_open: Optional[str] = None
-    sepa_fields: Optional[str] = None
-    batch_reference: Optional[str] = None
-    financial_account_id: Optional[str] = None
-    currency: Optional[str] = None
-    original_amount: Optional[str] = None
-    created_at: Optional[str] = None
-    updated_at: Optional[str] = None
-    version: Optional[str] = None
-    financial_statement_id: Optional[str] = None
-    processed_at: Optional[str] = None
-    account_servicer_transaction_id: Optional[str] = None
-    payments: List = field(default_factory=list)
-    ledger_account_bookings: List = field(default_factory=list)
+from .model import MoneybirdModel
+from .financial_mutation import FinancialMutation
 
 
 @dataclass
@@ -47,3 +17,14 @@ class FinancialStatement(MoneybirdModel):
     official_balance: Optional[str] = None
     importer_service: Optional[str] = None
     financial_mutations: List[FinancialMutation] = field(default_factory=list)
+
+    def load(self, id: int) -> None:
+        raise NotImplementedError(
+            "Financial statements cannot be loaded from Moneybird"
+        )
+
+    @classmethod
+    def find_by_id(cls: type[Self], id: int) -> Self:
+        raise NotImplementedError(
+            "Financial statements cannot be loaded from Moneybird"
+        )
