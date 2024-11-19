2101d3732fb59ea284896a365366bbccd69bfb8e
TimEvci <tevci93@gmail.com>
2024-11-19 14:32:25 +0300

    test: add unit tests for Payment class with fixture for payment data

---
+++ tests/test_payment.py
@@ -0,0 +1,40 @@
+import pytest
+from moneysnake.payment import Payment
+from pytest_mock import MockType
+
+
+@pytest.fixture(name="payment_data")
+def fixture_payment_data():
+    return {
+        "id": "433546310070568441",
+        "administration_id": 123,
+        "invoice_type": "SalesInvoice",
+        "invoice_id": "433546309874484716",
+        "financial_account_id": "433546310057985525",
+        "user_id": 1,
+        "payment_transaction_id": None,
+        "transaction_identifier": None,
+        "price": "363.0",
+        "price_base": "363.0",
+        "payment_date": "2024-09-30",
+        "credit_invoice_id": None,
+        "financial_mutation_id": "433546310065325560",
+        "ledger_account_id": "433546310050645492",
+        "linked_payment_id": None,
+        "manual_payment_action": None,
+        "created_at": "2024-09-30T07:40:02.576Z",
+        "updated_at": "2024-09-30T07:40:02.576Z",
+    }
+
+
+def test_payment(payment_data: dict, mocker: MockType):
+
+    payment = Payment.from_dict(payment_data)
+
+    assert payment.payment_date == "2024-09-30"
+
+    with pytest.raises(NotImplementedError):
+        payment.save()
+
+    with pytest.raises(NotImplementedError):
+        payment.delete()
