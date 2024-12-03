c3229dbe936d1688a884f7c74a95337348e6ee66
TimEvci <tevci93@gmail.com>
2024-12-03 11:04:02 +0100

    feat: implement save method for FinancialStatement to handle API interactions

--- moneysnake/financial_statement.py
+++ moneysnake/financial_statement.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 
 from .model import MoneybirdModel
 from .financial_mutation import FinancialMutation
+from .client import post_request
 
 
 @dataclass
@@ -17,6 +18,32 @@ class FinancialStatement(MoneybirdModel):
     official_balance: Optional[str] = None
     importer_service: Optional[str] = None
     financial_mutations: List[FinancialMutation] = field(default_factory=list)
+
+    def save(self) -> None:
+        """
+        Save the external sales invoice. Overrides the save method in MoneybirdModel.
+        """
+        financial_statement_data = self.to_dict()
+        # For the POST and PATCH requests we need to use the details_attributes key
+        # instead of details key to match the Moneybird API.
+        financial_statement_data["financial_mutation_attributes"] = (
+            financial_statement_data.pop("financial_mutations", [])
+        )
+
+        if self.id is None:
+            data = post_request(
+                f"{self.endpoint}s",
+                data={self.endpoint: financial_statement_data},
+                method="post",
+            )
+            self.update(data)
+        else:
+            data = post_request(
+                f"{self.endpoint}s/{self.id}",
+                data={self.endpoint: financial_statement_data},
+                method="patch",
+            )
+            self.update(data)
 
     def load(self, id: int) -> None:
         raise NotImplementedError(
